a07bf2ee64d152e93764c52ff7a66b34e2da65d9
Kurt McAlpine <kurt@movio.co>
2018-05-14 10:01:28 +1200

    Case insensitive matching of Last-Modified header
    This is to support http2 https://tools.ietf.org/html/rfc7540#section-8.1.2

--- assets/common.sh
+++ assets/common.sh
@@ -4,7 +4,7 @@ set -e
 # e.g. curl -R -I $1
 check_version() {
   # retrieves HTTP header of file URL response
-  local httpHeader=$(curl -R -I $1 2>&1 | grep 'Last-Modified:')
+  local httpHeader=$(curl -R -I $1 2>&1 | grep '[Ll]ast-[Mm]odified:')
   # Checks if field "Last-Modified" exists in HTTP header and transform it into timestamp string
   # if that field is not present, return current timestamp
   local dateVersionFormat="%Y%m%d%H%S"
@@ -13,7 +13,7 @@ check_version() {
   if [ ! -z "$httpHeader" ]
   then
         # echo "Last-Modified information returned for targeted file. Extract date, removing day of the week string
-        local tmpDateString=$(echo "$httpHeader" | sed -e "s/Last-Modified: //" | cut -d',' -f 2)
+        local tmpDateString=$(echo "$httpHeader" | sed -e "s/[Ll]ast-[Mm]odified: //" | cut -d',' -f 2)
         # rfc1123Format="%d\ %b\ %Y\ %H:%M:%S\ GMT" - in order to work in boot2docker, it has to be outside of quotes in the command below
         local dateString=$(date +"$dateVersionFormat" -D %d\ %b\ %Y\ %H:%M:%S\ GMT -d "$tmpDateString")
   fi
