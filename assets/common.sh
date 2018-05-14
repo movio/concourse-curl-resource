ded0a5d5af5bcde0fc07bd598f8f718a662afd33
Kurt McAlpine <kurt@movio.co>
2018-05-14 12:08:02 +1200

    Remove trailing whitespace

--- assets/common.sh
+++ assets/common.sh
@@ -5,7 +5,7 @@ set -e
 check_version() {
 
   # retrieves HTTP header of file URL response
-  httpHeader=$(curl -R -I "$1" 2>&1 | grep '[Ll]ast-[Mm]odified:')
+  httpHeader=$(curl -R -I "$1" 2>&1 | grep '[Ll]ast-[Mm]odified:' | sed 's/[ \t\n\r]*$//')
 
   if [ ! -z "$httpHeader" ]
   then
